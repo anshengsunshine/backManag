b06d91c53aa001bd48838d9dd90b24f8c410a2e7
jiajing <jiajing@zhiweidata.com>
2022-09-15 10:18:16 +0800

    ci(vue-router): 引入Vue-Router,并做相关配置，完成测试

---
+++ src/views/login/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Login Page</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style></style>
