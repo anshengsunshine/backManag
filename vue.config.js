731bc9ae7aaea22d3fd41d1259a7b5fcc09ae525
jiajing <jiajing@zhiweidata.com>
2022-09-15 22:02:23 +0800

    ci(element-plus): 引入element-plus,并做自动化按需引入配置

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
-  outputDir: './build',
   //方式一：直接通过CLI提供给我们的选项来配置：
   // configureWebpack: {
   //   resolve: {
@@ -11,16 +13,27 @@ module.exports = {
   //   }
   // }
   //方式二：通过configureWebpack修改webpack的配置：
-  // configureWebpack: (config) => {
-  //   config.resolve.alias = {
-  //     '@': path.resolve(__dirname, 'src'),
-  //     views: '@/views'
-  //   }
-  // },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        views: '@/views'
+      }
+    },
+    //配置webpack自动按需引入element-plus，
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  },
   //方式三：通过chainWebpack修改webpack的配置
-  chainWebpack: (config) => {
-    config.resolve.alias
-      .set('@', path.resolve(__dirname, 'src'))
-      .set('views', '@/views')
-  }
+  // chainWebpack: (config) => {
+  //   config.resolve.alias
+  //     .set('@', path.resolve(__dirname, 'src'))
+  //     .set('views', '@/views')
+  // }
 }
