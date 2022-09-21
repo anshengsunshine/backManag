2a369b514471b6d206d4b21f5c67c45869b269e6
jiajing <jiajing@zhiweidata.com>
2022-09-22 05:52:37 +0800

    feat(user): 新增展示用户信息的组件

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="nav-header">
-    <div class="fold-menu" @click="handleFoldClick">
-      <i-ep-Fold v-if="isFold" class="el-icon-s-fold" />
-      <i-ep-Expand v-else class="el-icon-s-unfold" />
+    <i
+      class="fold-menu"
+      :class="isFold ? 'el-icon-s-fold' : 'el-icon-s-unfold'"
+      @click="handleFoldClick"
+    ></i>
+    <div class="content">
+      <div>面包屑</div>
+      <user-info />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import UserInfo from './user-info.vue'
 
 export default defineComponent({
+  components: {
+    UserInfo
+  },
   emits: ['foldChange'],
   setup(props, { emit }) {
     const isFold = ref(false)
@@ -29,9 +38,20 @@ export default defineComponent({
 
 <style scoped lang="less">
 .nav-header {
+  display: flex;
+  width: 100%;
+
   .fold-menu {
     font-size: 30px;
     cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 20px;
   }
 }
 </style>
